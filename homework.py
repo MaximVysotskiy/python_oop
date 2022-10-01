9ba80d043e7f03b3ea04b36f26f74931c76f7c09
MaximVysotskiy <114359367+MaximVysotskiy@users.noreply.github.com>
2022-10-01 22:26:59 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -50,13 +50,11 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        return InfoMessage(
-                           self.__class__.__name__,
+        return InfoMessage(self.__class__.__name__,
                            self.duration,
                            self.get_distance(),
                            self.get_mean_speed(),
-                           self.get_spent_calories()
-                          )
+                           self.get_spent_calories())
 
 
 class Running(Training):
